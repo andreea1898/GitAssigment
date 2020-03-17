2356771a4c715afb7fd45a5b2cf1c8071fe01765
andre <andre@DESKTOP-LPODVTM>
2020-03-17 20:24:28 +0200

    First commit

---
+++ tema_Git/src/ro/ase/acs/cts/temaGit/TestGit.java
@@ -0,0 +1,8 @@
+package ro.ase.acs.cts.temaGit;
+
+public class TestGit {
+	public static void main(String[] args) {
+		System.out.println("Hello Git! Badea Andreea Aplicatie web pentru gestiunea unei librarii online");
+	}
+
+}
